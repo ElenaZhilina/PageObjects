63015b9a8e558ac8a28edb245f69a9fff2ddebd5
Elena <elenazhilina@yandex.ru>
2024-06-02 19:52:31 +0500

    DataGen modified

--- src/test/java/ru/netology/data/DataGen.java
+++ src/test/java/ru/netology/data/DataGen.java
@@ -2,30 +2,60 @@ package ru.netology.data;
 
 import lombok.Value;
 
+import java.util.Random;
+
 public class DataGen {
     private DataGen() {
     }
 
+
     @Value
     public static class User {
-        private String login;
-        private String pwd;
+        String login;
+        String pwd;
     }
 
     public static User getUser() {
-        return new User("Helen", "qwerty1111");
+        return new User("vasya", "qwerty123");
+    }
+
+    public static String getCode() {
+        return "12345";
+    }
+
+    public static Card getFirstCard() {
+        return new Card("5559 0000 0000 0001","92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static Card getSecondCard() {
+        return new Card("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
     public static User getOtherUser(User original) {
         return new User("Anrew", "1111qwerty");
     }
 
-    @Value
-    public static class Code {
-        private String code;
+    public static String maskedCardNumber(String cardNumber) {
+        return "**** **** **** " + cardNumber.substring(12);
     }
 
-    public static Code getCodeFor(User userInfo) {
-        return new Code("12345");
+    public static int validBalance(int balance) {
+        return new Random().nextInt(Math.abs(balance)) + 1;
     }
+
+    public static int invalidBalance(int balance) {
+        return Math.abs(balance) + new Random().nextInt(10000);
+    }
+
+
+    @Value
+    public static class Card {
+        String cardNum;
+        String testId;
+    }
+
+//    public static Code getCodeFor(User userInfo) {
+//        return new Code("12345");
+
+
 }
--- src/test/java/ru/netology/test/TransferTest.java
+++ src/test/java/ru/netology/test/TransferTest.java
@@ -1,15 +1,24 @@
 package ru.netology.test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-
 import org.junit.jupiter.api.Test;
+import ru.netology.data.DataGen;
 
 public class TransferTest {
 
     @Test
     void moneyTransferToCard() {
         open("http://localhost:9999");
+
+        var user = DataGen.getUser();
+        $("[data-test-id=login] input").setValue(user.getLogin());
+        $("[data-test-id=password] input").setValue(user.getPwd());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=code] input").shouldBe(visible);
+
+
 
     }
 
